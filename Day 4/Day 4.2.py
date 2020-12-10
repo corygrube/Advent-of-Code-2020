0bda80acea38a13187a0c3b6531d1095c2342b1f
Cory Grube <cory.grube@gmail.com>
2020-12-09 23:22:55 -0500

    Initial commit

---
+++ Day 4/Day 4.2.py
@@ -0,0 +1,52 @@
+import pprint
+# read values from data file
+file_path = 'Day 4/Data.txt'
+with open(file_path, 'r') as file:
+	file_read = file.read()
+
+#split into list on double line break
+strings = file_read.split('\n\n')
+# separate key:value pairs by ' '. Removes misc line breaks.
+# generate list of dictionary
+passports = []
+for string in strings:
+	passport_str = string.replace('\n', ' ')
+	passport = {
+		s.split(':')[0] : s.split(':')[1] for s in passport_str.split(' ')
+	}
+	passports.append(passport)
+
+# check required keys. Assumptions:
+# 	8=valid
+# 	7=valid if cid is the missing key
+# 	no passports will have more than 8 keys
+valid = 0
+for passport in passports:
+	length = len(passport)
+	if length <= 6:
+		continue
+	elif length == 7:
+		if 'cid' not in passport:
+			valid += 1
+	else:
+		valid += 1
+print(valid)
+
+# valid = 0
+# for passport in passports:
+# 	if 'byr' not in passport:
+# 		continue
+# 	if 'iyr' not in passport:
+# 		continue
+# 	if 'eyr' not in passport:
+# 		continue
+# 	if 'hgt' not in passport:
+# 		continue
+# 	if 'hcl' not in passport:
+# 		continue
+# 	if 'ecl' not in passport:
+# 		continue
+# 	if 'pid' not in passport:
+# 		continue
+# 	valid += 1
+# print(valid)
